4e0c0f362fe7483d5bb3a1a5e0c8cd85736e30ae
Eric Holma <furiated@gmail.com>
2025-08-19 22:35:48 -0400

    Update the comments in the JavaScript explaining the functionality

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ const generateCatFact = async () => {
     const response = await fetch(url);
     // Check that the response is okay
     if (response.ok) {
+      // If the response is valid, convert the response to json format
       const jsonResponse = await response.json();
+      // Update the HTML in the cat fact box to add the text from the cat fact response
       catFactBox.innerHTML = `<p>${jsonResponse.fact}</p>`;
     }
     // Log any error to the console
